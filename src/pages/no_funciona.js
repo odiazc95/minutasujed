57c46c92607352612cab2824cc9f8648d0ff43fa
Emmanuel Bayona <Bayonaj368@gmail.com>
2023-07-09 17:49:48 -0600

    styel: not found page

--- src/pages/no_funciona.js
+++ src/pages/no_funciona.js
@@ -3,7 +3,7 @@ import { Metric } from "@tremor/react";
 
 const NotFound = () => {
   return (
-    <section className='w-full h-full flex flex-col justify-center items-center gap-4'>
+    <section className='w-full h-screen flex flex-col justify-center items-center gap-4'>
       <img src={logo_UJED} alt="ujed logo" className='bg-main rounded-full w-52 h-52 -mt-4' />
       <Metric className='text-center'>Lo sentimos, esta página no existe</Metric>
     </section>
