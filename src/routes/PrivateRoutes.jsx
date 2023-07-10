27914e498d41ab9e7a5566bc8eb6416322672b80
Emmanuel Bayona <Bayonaj368@gmail.com>
2023-07-09 23:33:26 -0600

    fix: priv and public routes

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -3,8 +3,8 @@ import Cookies from 'js-cookie';
 
 const PrivateRoutes = ({ children }) => {
     
-    // const idU = Cookies.get('idUser');
-    const idU = 1;
+    const idU = Cookies.get('idUser');
+    // const idU = 1;
 
     // if the user ID is defined in the cookies, the dashboard content is shown
     // if not, the login form is shown
